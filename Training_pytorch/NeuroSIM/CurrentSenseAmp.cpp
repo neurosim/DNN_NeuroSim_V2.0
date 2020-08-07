a44d29a2cf854bcfa1099269cf618d65db0e99d3
NeuroSim <ysm1030@gmail.com>
2020-08-06 22:47:51 -0400

    Add files via upload

--- Training_pytorch/NeuroSIM/CurrentSenseAmp.cpp
+++ Training_pytorch/NeuroSIM/CurrentSenseAmp.cpp
@@ -86,7 +86,7 @@ void CurrentSenseAmp::CalculateUnitArea() {
 		CalculateGateArea(INV, 1, widthNmos, 0, tech.featureSize*MAX_TRANSISTOR_HEIGHT, tech, &hNmosS, &wNmosS);
 		CalculateGateArea(INV, 1, 0, widthPmos, tech.featureSize*MAX_TRANSISTOR_HEIGHT, tech, &hPmos, &wPmos);
 		
-		areaUnit = (hNmos*wNmos)*48 + (hPmos*wPmos)*24;
+		areaUnit = (hNmos*wNmos)*48 + (hPmos*wPmos)*40;
 	}
 }
 
--- Training_pytorch/NeuroSIM/MultilevelSenseAmp.cpp
+++ Training_pytorch/NeuroSIM/MultilevelSenseAmp.cpp
@@ -167,14 +167,27 @@ void MultilevelSenseAmp::CalculatePower(const vector<double> &columnResistance, 
 	} else {
 		leakage = 0;
 		readDynamicEnergy = 0;
-		for (double i=0; i<columnResistance.size(); i++) {
-			double E_Col = 0;
-			E_Col = GetColumnPower(columnResistance[i]);
-			if (columnResistance[i] == columnResistance[i]) {
-				readDynamicEnergy += E_Col;
+		
+		double LatencyCol = 0;
+		for (double j=0; j<columnResistance.size(); j++){
+			double T_Col = 0;
+			T_Col = GetColumnLatency(columnResistance[j]);
+			if (columnResistance[j] == columnResistance[j]) {
+				LatencyCol = max(LatencyCol, T_Col);
 			} else {
-				readDynamicEnergy += 0;
+				LatencyCol = LatencyCol;
 			}
+			if (LatencyCol < 1e-9) {
+				LatencyCol = 1e-9;
+			} else if (LatencyCol > 10e-9) {
+				LatencyCol = 10e-9;
+			}
+		}
+
+		for (double i=0; i<columnResistance.size(); i++) {
+			double P_Col = 0;
+			P_Col = GetColumnPower(columnResistance[i]);
+			readDynamicEnergy += MAX(P_Col*LatencyCol, 0);
 		}
 		readDynamicEnergy *= numRead;
 	}
@@ -292,8 +305,7 @@ double MultilevelSenseAmp::GetColumnLatency(double columnRes) {
 
 double MultilevelSenseAmp::GetColumnPower(double columnRes) {
 	double Column_Power = 0;
-	double Column_Energy = 0;
-
+	
 	if ((double) 1/columnRes == 0) { 
 		Column_Power = 1e-6;
 	} else if (columnRes == 0) {
@@ -301,184 +313,63 @@ double MultilevelSenseAmp::GetColumnPower(double columnRes) {
 	} else {
 		if (param->deviceroadmap == 1) {  // HP
 			if (param->technode == 130) {
-				Column_Power = 19.898*1e-6;
+				Column_Power = 19.898*(levelOutput-1)*1e-6;
+				Column_Power += 0.17452*exp(-2.367*log10(columnRes));
 			} else if (param->technode == 90) {
-				Column_Power = 13.09*1e-6;
+				Column_Power = 13.09*(levelOutput-1)*1e-6;
+				Column_Power += 0.14900*exp(-2.345*log10(columnRes));
 			} else if (param->technode == 65) {
-				Column_Power = 9.9579*1e-6;
+				Column_Power = 9.9579*(levelOutput-1)*1e-6;
+				Column_Power += 0.1083*exp(-2.321*log10(columnRes));
 			} else if (param->technode == 45) {
-				Column_Power = 7.7017*1e-6;
+				Column_Power = 7.7017*(levelOutput-1)*1e-6;
+				Column_Power += 0.0754*exp(-2.296*log10(columnRes));
 			} else if (param->technode == 32){  
-				Column_Power = 3.9648*1e-6;
+				Column_Power = 3.9648*(levelOutput-1)*1e-6;
+				Column_Power += 0.079*exp(-2.313*log10(columnRes));
 			} else if (param->technode == 22){   
-				Column_Power = 1.8939*1e-6;
+				Column_Power = 1.8939*(levelOutput-1)*1e-6;
+				Column_Power += 0.073*exp(-2.311*log10(columnRes));
 			} else if (param->technode == 14){  
-				Column_Power = 1.2*1e-6;
+				Column_Power = 1.2*(levelOutput-1)*1e-6;
+				Column_Power += 0.0584*exp(-2.311*log10(columnRes));
 			} else if (param->technode == 10){  
-				Column_Power = 0.8*1e-6;
+				Column_Power = 0.8*(levelOutput-1)*1e-6;
+				Column_Power += 0.0318*exp(-2.311*log10(columnRes));
 			} else {   // 7nm
-				Column_Power = 0.5*1e-6;
+				Column_Power = 0.5*(levelOutput-1)*1e-6;
+				Column_Power += 0.0210*exp(-2.311*log10(columnRes));
 			}
 		} else {                         // LP
 			if (param->technode == 130) {
-				Column_Power = 18.09*1e-6;
+				Column_Power = 18.09*(levelOutput-1)*1e-6;
+				Column_Power += 0.1380*exp(-2.303*log10(columnRes));
 			} else if (param->technode == 90) {
-				Column_Power = 12.612*1e-6;
+				Column_Power = 12.612*(levelOutput-1)*1e-6;
+				Column_Power += 0.1023*exp(-2.303*log10(columnRes));
 			} else if (param->technode == 65) {
-				Column_Power = 8.4147*1e-6;
+				Column_Power = 8.4147*(levelOutput-1)*1e-6;
+				Column_Power += 0.0972*exp(-2.303*log10(columnRes));
 			} else if (param->technode == 45) {
-				Column_Power = 6.3162*1e-6;
+				Column_Power = 6.3162*(levelOutput-1)*1e-6;
+				Column_Power += 0.075*exp(-2.303*log10(columnRes));
 			} else if (param->technode == 32){  
-				Column_Power = 3.0875*1e-6;
+				Column_Power = 3.0875*(levelOutput-1)*1e-6;
+				Column_Power += 0.0649*exp(-2.297*log10(columnRes));
 			} else if (param->technode == 22){   
-				Column_Power = 1.7*1e-6;
+				Column_Power = 1.7*(levelOutput-1)*1e-6;
+				Column_Power += 0.0631*exp(-2.303*log10(columnRes));
 			} else if (param->technode == 14){   
-				Column_Power = 1.0*1e-6;
+				Column_Power = 1.0*(levelOutput-1)*1e-6;
+				Column_Power += 0.0508*exp(-2.303*log10(columnRes));
 			} else if (param->technode == 10){   
-				Column_Power = 0.55*1e-6;
+				Column_Power = 0.55*(levelOutput-1)*1e-6;
+				Column_Power += 0.0315*exp(-2.303*log10(columnRes));
 			} else {   // 7nm
-				Column_Power = 0.35*1e-6;
+				Column_Power = 0.35*(levelOutput-1)*1e-6;
+				Column_Power += 0.0235*exp(-2.303*log10(columnRes));
 			}
 		}
 	}
-	
-	double up_bound = 3, mid_bound = 1.1, low_bound = 0.9;
-	double T_max = 0;
-	if (((double) 1/columnRes == 0) || (columnRes == 0)) {
-		Column_Energy += 0;
-	} else {
-		if (param->deviceroadmap == 1) {  // HP
-			Column_Energy = Column_Power*1e-9*(levelOutput-1);
-		} else {                         // LP
-			if (param->technode == 130) {
-				T_max = (0.2679*log(columnRes/1000)+0.0478)*1e-9;   // T_max = (0.2679*log(R_BL/1000)+0.0478)*10^-9;
-
-				for (int i=1; i<levelOutput-1; i++){
-					double ratio = Rref[i]/columnRes;
-					double T = 0;
-					if (ratio >= 20 || ratio <= 0.05) {
-						T = 1e-9;
-					} else {
-						if (ratio <= low_bound){
-							T = T_max * (3.915*pow(ratio,3)-5.3996*pow(ratio,2)+2.4653*ratio+0.3856);  // y = 3.915*x^3-5.3996*x^2+2.4653*x+0.3856;
-						} else if (mid_bound <= ratio <= up_bound){
-							T = T_max * (0.0004*pow(ratio,4)-0.0087*pow(ratio,3)+0.0742*pow(ratio,2)-0.2725*ratio+1.2211);  // y = 0.0004*x^4-0.0087*x^3+0.0742*x^2-0.2725*x+1.2211;
-						} else if (ratio>up_bound){
-							T = T_max * (0.0004*pow(ratio,4)-0.0087*pow(ratio,3)+0.0742*pow(ratio,2)-0.2725*ratio+1.2211);
-						} else {
-							T = T_max;
-						}
-					}
-					Column_Energy += Column_Power*T;
-				}
-			} else if (param->technode == 90) {
-				T_max = (0.0586*log(columnRes/1000)+1.41)*1e-9;   // T_max = (0.0586*log(R_BL/1000)+1.41)*10^-9;
-
-				for (int i=1; i<levelOutput-1; i++){
-					double ratio = Rref[i]/columnRes;
-					double T = 0;
-					if (ratio >= 20 || ratio <= 0.05) {
-						T = 1e-9;
-					} else {
-						if (ratio <= low_bound){
-							T = T_max * (3.726*pow(ratio,3)-5.651*pow(ratio,2)+2.8249*ratio+0.3574);    // y = 3.726*x^3-5.651*x^2+2.8249*x+0.3574;
-						} else if (mid_bound <= ratio <= up_bound){
-							T = T_max * (0.0000008*pow(ratio,4)-0.00007*pow(ratio,3)+0.0017*pow(ratio,2)-0.0188*ratio+0.9835);  // y = 0.0000008*x^4-0.00007*x^3+0.0017*x^2-0.0188*x+0.9835;
-						} else if (ratio>up_bound){
-							T = T_max * (0.0000008*pow(ratio,4)-0.00007*pow(ratio,3)+0.0017*pow(ratio,2)-0.0188*ratio+0.9835);
-						} else {
-							T = T_max;
-						}
-					}
-					Column_Energy += Column_Power*T;
-				}
-			} else if (param->technode == 65) {
-				T_max = (0.1239*log(columnRes/1000)+0.6642)*1e-9;   // T_max = (0.1239*log(R_BL/1000)+0.6642)*10^-9;
-
-				for (int i=1; i<levelOutput-1; i++){
-					double ratio = Rref[i]/columnRes;
-					double T = 0;
-					if (ratio >= 20 || ratio <= 0.05) {
-						T = 1e-9;
-					} else {
-						if (ratio <= low_bound){
-							T = T_max * (1.3899*pow(ratio,3)-2.6913*pow(ratio,2)+2.0483*ratio+0.3202);    // y = 1.3899*x^3-2.6913*x^2+2.0483*x+0.3202;
-						} else if (mid_bound <= ratio <= up_bound){
-							T = T_max * (0.0036*pow(ratio,4)-0.0363*pow(ratio,3)+0.1043*pow(ratio,2)-0.0346*ratio+1.0512);   // y = 0.0036*x^4-0.0363*x^3+0.1043*x^2-0.0346*x+1.0512;
-						} else if (ratio>up_bound){
-							T = T_max * (0.0036*pow(ratio,4)-0.0363*pow(ratio,3)+0.1043*pow(ratio,2)-0.0346*ratio+1.0512);
-						} else {
-							T = T_max;
-						}
-					}
-					Column_Energy += Column_Power*T;
-				}
-			} else if (param->technode == 45 || param->technode == 32) {
-				T_max = (0.0714*log(columnRes/1000)+0.7651)*1e-9;    // T_max = (0.0714*log(R_BL/1000)+0.7651)*10^-9;
-
-				for (int i=1; i<levelOutput-1; i++){
-					double ratio = Rref[i]/columnRes;
-					double T = 0;
-					if (ratio >= 20 || ratio <= 0.05) {
-						T = 1e-9;
-					} else {
-						if (ratio <= low_bound){
-							T = T_max * (3.7949*pow(ratio,3)-5.6685*pow(ratio,2)+2.6492*ratio+0.4807);    // y = 3.7949*x^3-5.6685*x^2+2.6492*x+0.4807
-						} else if (mid_bound <= ratio <= up_bound){
-							T = T_max * (0.000001*pow(ratio,4)-0.00006*pow(ratio,3)+0.0001*pow(ratio,2)-0.0171*ratio+1.0057);   // 0.000001*x^4-0.00006*x^3+0.0001*x^2-0.0171*x+1.0057;
-						} else if (ratio>up_bound){
-							T = T_max * (0.000001*pow(ratio,4)-0.00006*pow(ratio,3)+0.0001*pow(ratio,2)-0.0171*ratio+1.0057);
-						} else {
-							T = T_max;
-						}
-					}
-					Column_Energy += Column_Power*T;
-				}
-			} else {   // technode below and equal to 22nm
-				Column_Energy += Column_Power*1e-9*(levelOutput-1);
-			}
-		}
-	}
-	if (param->deviceroadmap == 1) {  // HP			
-		if (param->technode == 130) {
-			Column_Energy += 0.207452*exp(-2.367*log10(columnRes))*1e-9;
-		} else if (param->technode == 90) {
-			Column_Energy += 0.164900*exp(-2.345*log10(columnRes))*1e-9;
-		} else if (param->technode == 65) {
-			Column_Energy += 0.128483*exp(-2.321*log10(columnRes))*1e-9;
-		} else if (param->technode == 45) {
-			Column_Energy += 0.097754*exp(-2.296*log10(columnRes))*1e-9;
-		} else if (param->technode == 32){  
-			Column_Energy += 0.083709*exp(-2.313*log10(columnRes))*1e-9;
-		} else if (param->technode == 22){   
-			Column_Energy += 0.084273*exp(-2.311*log10(columnRes))*1e-9;
-		} else if (param->technode == 14){  
-			Column_Energy += 0.060584*exp(-2.311*log10(columnRes))*1e-9;
-		} else if (param->technode == 10){  
-			Column_Energy += 0.049418*exp(-2.311*log10(columnRes))*1e-9;
-		} else {   // 7nm
-			Column_Energy += 0.040310*exp(-2.311*log10(columnRes))*1e-9;
-		}
-	} else {                         // LP
-		if (param->technode == 130) {
-			Column_Energy += 0.169380*exp(-2.303*log10(columnRes))*1e-9;
-		} else if (param->technode == 90) {
-			Column_Energy += 0.144323*exp(-2.303*log10(columnRes))*1e-9;
-		} else if (param->technode == 65) {
-			Column_Energy += 0.121272*exp(-2.303*log10(columnRes))*1e-9;
-		} else if (param->technode == 45) {
-			Column_Energy += 0.100225*exp(-2.303*log10(columnRes))*1e-9;
-		} else if (param->technode == 32){  
-			Column_Energy += 0.079449*exp(-2.297*log10(columnRes))*1e-9;
-		} else if (param->technode == 22){   
-			Column_Energy += 0.072341*exp(-2.303*log10(columnRes))*1e-9;
-		} else if (param->technode == 14){   
-			Column_Energy += 0.061085*exp(-2.303*log10(columnRes))*1e-9;
-		} else if (param->technode == 10){   
-			Column_Energy += 0.051580*exp(-2.303*log10(columnRes))*1e-9;
-		} else {   // 7nm
-			Column_Energy += 0.043555*exp(-2.303*log10(columnRes))*1e-9;
-		}
-	}	
-	return Column_Energy;
+	return Column_Power;
 }
